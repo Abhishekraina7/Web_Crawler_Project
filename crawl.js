c3cf5e554e40686611f4e253fc9301c62d2b95ba
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-08-26 22:46:43 +0530

    1. Corrected the normalizeURL function return value. 2. Created the getURLFromHTML function which extrack URLs from different HTML tags.

--- crawl.js
+++ crawl.js
@@ -1,13 +1,56 @@
 
 export { normalizeURL }; // this makes the normalizeURL function available to be used in other modules using 'import'
-
-
+export { getURLsFromHTML };
 
 function normalizeURL(url) {
     const urlObject = new URL(url);
     const protocol = urlObject.protocol === 'http:' ? 'http://' : 'https://';
     const pathname = urlObject.pathname.replace(/\/$/, ''); // remove trailing slash
     const hostname = urlObject.hostname;
-    return `${protocol}${hostname}${pathname}`;
+    return `${hostname}${pathname}`;
+}
+
+
+//This function takes a string of HTML as input and returns a list of all the link URLs.
+
+const jsdom = require('jsdom');
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const dom = new jsdom.JSDOM(htmlBody);
+    const urls = [];
+
+    // Extract URLs from <a> tags
+    dom.window.document.querySelectorAll('a').forEach((anchor) => {
+        const href = anchor.getAttribute('href');
+        if (href) {
+            urls.push(new URL(href, baseURL).href);
+        }
+    });
+
+    // Extract URLs from <img> tags
+    dom.window.document.querySelectorAll('img').forEach((img) => {
+        const src = img.getAttribute('src');
+        if (src) {
+            urls.push(new URL(src, baseURL).href);
+        }
+    });
+
+    // Extract URLs from <script> tags
+    dom.window.document.querySelectorAll('script').forEach((script) => {
+        const src = script.getAttribute('src');
+        if (src) {
+            urls.push(new URL(src, baseURL).href);
+        }
+    });
+
+    // Extract URLs from <link> tags
+    dom.window.document.querySelectorAll('link').forEach((link) => {
+        const href = link.getAttribute('href');
+        if (href) {
+            urls.push(new URL(href, baseURL).href);
+        }
+    });
+
+    return urls;
 }
 
