179121e33cd14e1e5d8a359938e67b2eb3b40d43
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-08-26 22:47:30 +0530

    Wrote Tests for getURLFromHTML function which passed successfully.

--- crawl.test.js
+++ crawl.test.js
@@ -47,10 +47,49 @@ describe('normalizeURL', () => {
     });
 });
 
+//test for getURLsFromHTML function
 
-// describe('normalizeURL', () => {
-//     it('removes protocols (http/https)', () => {
-//         expect(normalizeURL('https://blog.boot.dev/path').toBe('blog.boot.dev/path'));
-//         expect(normalizeURL('http://blog.boot.dev/path').toBe('blog.boot.dev/path'));
-//     })
-// })
+const assert = require('assert');
+const { getURLsFromHTML } = require('./crawl');
+
+describe('getURLsFromHTML', () => {
+    it('converts relative URLs to absolute URLs', () => {
+        const htmlBody = '<a href="/about">About</a>';
+        const baseURL = 'https://example.com';
+        const expectedUrls = ['https://example.com/about'];
+        const actualUrls = getURLsFromHTML(htmlBody, baseURL);
+        assert.deepStrictEqual(actualUrls, expectedUrls);
+    });
+
+    it('finds all anchor elements in a body of HTML', () => {
+        const htmlBody = `
+      <body>
+        <a href="/about">About</a>
+        <p>Some text</p>
+        <a href="/contact">Contact</a>
+        <a href="/faq">FAQ</a>
+      </body>
+    `;
+        const baseURL = 'https://example.com';
+        const expectedUrls = [
+            'https://example.com/about',
+            'https://example.com/contact',
+            'https://example.com/faq',
+        ];
+        const actualUrls = getURLsFromHTML(htmlBody, baseURL);
+        assert.deepStrictEqual(actualUrls, expectedUrls);
+    });
+
+    it('ignores anchor elements with no href attribute', () => {
+        const htmlBody = `
+      <body>
+        <a>About</a>
+        <a href="/contact">Contact</a>
+      </body>
+    `;
+        const baseURL = 'https://example.com';
+        const expectedUrls = ['https://example.com/contact'];
+        const actualUrls = getURLsFromHTML(htmlBody, baseURL);
+        assert.deepStrictEqual(actualUrls, expectedUrls);
+    });
+});
